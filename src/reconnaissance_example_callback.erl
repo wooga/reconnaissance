be88db567c9d08d56857a8876120db9f86ed8055
odo <florian.odronitz@wooga.net>
2014-01-28 15:14:41 +0100

    more comments

--- src/reconnaissance_example_callback.erl
+++ src/reconnaissance_example_callback.erl
@@ -2,12 +2,16 @@
 
 -compile(export_all).
 
+% Creates the payload that is sent as multicast packet
 request() ->
     <<"hello">>.
 
+% Request is the payload from the request() function above
+% The output is the response payload
 response(_IP, _Port, Request) ->
     list_to_binary(atom_to_list(node())).
 
+% Called when the response from discoverd nodes is received
+% Output will be stored in the node list
 handle_response(IP, Port, Response) ->
     {IP, Port, Response}.
-
